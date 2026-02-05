b38dc7135daf3e3b6ea9c35b06c4a69ec91b0832
Sricharan <k.sricharanrao@gmail.com>
2026-02-05 14:00:36 +0530

    Add solution for Two Sum problem with O(N) time complexity

---
+++ LeetCode_Solutions/Easy/Two_Sum.py
@@ -0,0 +1,33 @@
+
+"""
+Problem: Two Sum
+Platform: LeetCode
+Difficultiy: Easy
+
+Time complexity: O(N)
+space COmplexity: O()
+"""
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        set_nums = set(nums)
+        lis=[]
+        for i, j in enumerate(nums):
+            
+            if(target-j in set_nums):
+                
+                try:
+                    idx = nums.index(target - j)
+                    if idx != i:
+                        return [i,nums.index(target-j)]
+                except:
+                    continue
+
+        return []
+        
+        
